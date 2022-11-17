ec1fd7f6c5966ce5e786c1c05282985d76031aca
Andrei Hutsau <mwplt@mail.ru>
2022-11-17 20:03:00 +0300

    made styled Components with props

--- src/CurrentBankomat.tsx
+++ src/CurrentBankomat.tsx
@@ -7,19 +7,37 @@ type CurrentBankomatPropsType = {
 }
 export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
     return (
-        props.money.banknotes === 'Dollars'
-            ? <BanknoteGreen>
-                <Value>{props.money.value} </Value>
-                <span>{props.money.banknotes} </span>
-                <span>{props.money.number}</span>
-            </BanknoteGreen>
-            : <BanknoteRed>
-                <Value>{props.money.value} </Value>
-                <span>{props.money.banknotes} </span>
-                <span>{props.money.number}</span>
-            </BanknoteRed>
+        <Banknote color={props.money.banknotes==="Dollars"?'lawngreen':'coral'}>
+            <Value>{props.money.value} </Value>
+            <span>{props.money.banknotes} </span>
+            <span>{props.money.number}</span>
+        </Banknote>
+        // props.money.banknotes === 'Dollars'
+        //     ? <BanknoteGreen>
+        //         <Value>{props.money.value} </Value>
+        //         <span>{props.money.banknotes} </span>
+        //         <span>{props.money.number}</span>
+        //     </BanknoteGreen>
+        //     : <BanknoteRed>
+        //         <Value>{props.money.value} </Value>
+        //         <span>{props.money.banknotes} </span>
+        //         <span>{props.money.number}</span>
+        //     </BanknoteRed>
     );
 };
+
+const Banknote = styled.div`
+  background-color: ${props=>{
+      if (props.color==='lawngreen'){
+          return 'lawngreen'
+      }else {
+          return 'coral'
+      }
+  }};
+  width: 200px;
+  height: 150px;
+  margin: 10px;
+`
 
 const BanknoteGreen = styled.div`
   background-color: lawngreen;
